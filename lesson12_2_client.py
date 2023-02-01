9eae5bc6020e82d91aa939a731245dfe2e0883fe
OlhaDot <121576147+OlhaDot@users.noreply.github.com>
2023-02-02 00:48:19 +0200

    Delete lesson12_2_client.py

--- lesson12_2_client.py
+++
@@ -1,8 +0,0 @@
-# 2 - server socket
-# client for server socket check
-import socket
-
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.connect(('localhost', 55000))
-    s.send(bytes('-2,4', encoding='UTF-8'))
-
